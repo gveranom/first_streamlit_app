b1fd41c6190a3fb15391c337e205b8e11e3a1d20
gveranom <125486134+gveranom@users.noreply.github.com>
2023-02-17 13:31:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,23 +26,34 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
+# New section Frutyvice to display API response
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  # streamlit.write('The user entered ', fruit_choice)
+  if not fruit_choice:
+    stramlit.error('Please select a fruit to get information')
+  else:
+    #Next line to do request fixed a fruit
+    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    #Next line to do request dinamic a fruit
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # Take the jason version of the response and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output it in the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+      
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
 
-#Next line to do request fixed a fruit
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#Next line to do request dinamic a fruit
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+
 # drop next line
 #streamlit.text(fruityvice_response.json()) # Just writes the  data to the screen
 
-# Take the jason version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it in the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
