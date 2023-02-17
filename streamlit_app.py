675ef9a0dd30310a69e11e02e5fafd435822da3b
gveranom <125486134+gveranom@users.noreply.github.com>
2023-02-17 14:03:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,31 +50,33 @@ try:
     streamlit.dataframe(back_from_fuction)
 except URLError as e:
   streamlit.error()
-      
-
-
-
-
 
 # drop next line
 #streamlit.text(fruityvice_response.json()) # Just writes the  data to the screen
 
-
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruit_load_list")
-#my_data_row = my_cur.fetchone() # Load just firts row
-my_data_rows = my_cur.fetchall() # Load all rows
+
 #streamlit.text("The fruit load list contains:")
 streamlit.header("The fruit load list contains:")
-# show juus first row
-# streamlit.dataframe(my_data_row) 
-# show all rows
-streamlit.dataframe(my_data_rows) 
 
+# snowflake-related fuctions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    reyurn my_cur.fetchall()
+    
+# Add a button to load the fruit 
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  #my_data_row = my_cur.fetchone() # Load just firts row
+  #my_data_rows = my_cur.fetchall() # Load all rows
+  my_data_rows = get_fruit_load_list()
+  # show juus first row
+  # streamlit.dataframe(my_data_row) 
+  # show all rows
+  streamlit.dataframe(my_data_rows) 
+
+streamlit.stop()
 streamlit.text("What fruit would you like to add?")
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','ggg')
 streamlit.write('Thanks for adding ', add_my_fruit)
